529bc65f15a763d0563ed6fe4007b7ee7a252b18
Nicolas de Almeida Mavroucakis <132313941+nicolasMavroucakis@users.noreply.github.com>
2025-05-07 23:54:07 -0300

    eu sou foda e o resto e moda

--- app/crud/firebaseConfig/index.tsx
+++ app/crud/firebaseConfig/index.tsx
@@ -8,7 +8,7 @@ function StartFirebase() {
         apiKey: "AIzaSyCvoKcCLr5IqAujT2cIvsRAfy4Dkvh9azo",
         authDomain: "pi20251.firebaseapp.com",
         projectId: "pi20251",
-        storageBucket: "pi20251.appspot.com",
+        storageBucket: "pi20251.firebasestorage.app",
         messagingSenderId: "652437593102",
         appId: "1:652437593102:web:ec10d188783f4af0ddf05f",
         measurementId: "G-2W1HSKJY1F"
